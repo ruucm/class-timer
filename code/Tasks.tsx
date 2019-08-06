109b4b1e85d92992a5095d1fb7ae1ece8f22e8c1
ruucm <ruucm.a@gmail.com>
2019-08-06 19:32:28 +0900

    update

--- code/Tasks.tsx
+++ code/Tasks.tsx
@@ -7,24 +7,26 @@ export function Tasks({ showTimer, timeAmount, ...props }) {
   return (
     <Stack width="100%" height={timeAmount * 5} gap={0} background="pink">
       <Timer timeAmount={timeAmount} />
-      <Task timeAmount={timeAmount} name="킥오프" time={30} />
+      <Task timeAmount={timeAmount} name="킥오프" time={10} />
       <Task
         timeAmount={timeAmount}
         name="프로그램 및 Extension 설치"
-        time={10}
+        time={20}
       />
       <Task timeAmount={timeAmount} name="Framer 시작하기" time={10} />
-      <Task timeAmount={timeAmount} name="Properties I" time={10} />
-      <Task timeAmount={timeAmount} name="Override" time={10} />
+      <Task timeAmount={timeAmount} name="Properties I" time={5} />
+      <Task timeAmount={timeAmount} name="Override" time={5} />
       <Task timeAmount={timeAmount} name="start-override" time={10} />
       <Task timeAmount={timeAmount} name="Animation" time={10} />
+      <Task timeAmount={timeAmount} name="@@Break Time@@" time={10} />
       <Task timeAmount={timeAmount} name="width" time={10} />
       <Task timeAmount={timeAmount} name="background" time={10} />
-      <Task timeAmount={timeAmount} name="height" time={10} />
+
       <Task timeAmount={timeAmount} name="top" time={10} />
       <Task timeAmount={timeAmount} name="scale-rotate-opacity" time={10} />
+      <Task timeAmount={timeAmount} name="height" time={15} />
       <Task timeAmount={timeAmount} name="onTap" time={10} />
-      <Task timeAmount={timeAmount} name="basic-card" time={10} />
+      <Task timeAmount={timeAmount} name="basic-card" time={15} />
       <Task timeAmount={timeAmount} name="overflow-top" time={10} />
       <Task timeAmount={timeAmount} name="text-color" time={10} />
     </Stack>
